a2a0e72260e8838687bb1df0a065b1ef3a683dc0
DaviMessias <davibastos451@gmail.com>
2022-11-01 14:15:07 -0300

    stap 16 corrigido

---
+++ controller/HelloPanel.controller.js
@@ -0,0 +1,33 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
+ ], 
+ 
+ function (Controller, MessageToast, Fragment) {
+    "use strict";
+ 
+    return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+       exibirBotao: function () {
+          // lê a mensagem do modelo i18n 
+          var oBundle = this.getView().getModel("i18n").getResourceBundle();
+          var sRecipient = this.getView().getModel().getProperty("/conteudo/nome");
+          var sMsg = oBundle.getText("olaMsg", [sRecipient]);
+          // Mostrar mensagem
+          MessageToast.show(sMsg);
+       },
+
+       aoAbrirDialog: function () {
+         
+            if(!this.pDialog) {
+                  this.pDialog = this.loadFragment({
+                        name: "sap.ui.demo.walkthrough.view.HelloDialog"
+                  });
+            }
+            this.pDialog.then(function(oDialog){
+                  oDialog.open();
+            });
+         }
+       
+    });
+ });
